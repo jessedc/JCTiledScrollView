ab087b4c51ee89690fa87cd172cdde517a3cf1a4
Jesse Collis <jesse@jcmultimedia.com.au>
2012-02-01 13:36:43 +1100

    Initial Commit

---
+++ Demo/JCTiledViewDemo/JCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JCAppDelegate.h
+//  JCTiledViewDemo
+//
+//  Created by Jesse Collis on 1/02/12.
+//  Copyright (c) 2012 JC Multimedia Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demo/JCTiledViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JCTiledViewDemo
+//
+//  Created by Jesse Collis on 1/02/12.
+//  Copyright (c) 2012 JC Multimedia Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([JCAppDelegate class]));
+  }
+}
