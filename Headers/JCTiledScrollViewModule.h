131b787195dbbb41d6dc01a0a230c6b193be5c07
Jesse Collis <jesse@jcmultimedia.com.au>
2017-09-28 16:51:38 +1000

    Implement Objective C module map file for JCTiledScrollView static lib
    You can now use @import JCTiledScrollView in ObjC/Swift

---
+++ Headers/JCTiledScrollViewModule.h
@@ -0,0 +1,24 @@
+//
+//  JCTiledScrollViewModule.h
+//  JCTiledScrollView
+//
+//  Created by Jesse Collis on 28/9/17.
+//
+
+#ifndef JCTiledScrollViewModule_h
+#define JCTiledScrollViewModule_h
+
+#import <UIKit/UIKit.h>
+
+#import "ADAnnotationTapGestureRecognizer.h"
+#import "CGPDFDocument.h"
+#import "JCAnnotation.h"
+#import "JCAnnotationView.h"
+#import "JCPDFTiledView.h"
+#import "JCTiledLayer.h"
+#import "JCTiledPDFScrollView.h"
+#import "JCTiledScrollView.h"
+#import "JCTiledView.h"
+#import "JCVisibleAnnotationTuple.h"
+
+#endif /* JCTiledScrollViewModule_h */
